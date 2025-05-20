af66ff2d4a3ddead3fc590e6379ed40bf44bb6e6
Diwakar-Bhardwaj <diwakarraj1245@gmail.com>
2025-05-20 16:01:01 +0530

    Update UpdatePassword.jsx

--- frontend/src/components/UpdatePassword.jsx
+++ frontend/src/components/UpdatePassword.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/procted/update', {
+      const response = await fetch('https://appointment-service-1.onrender.com/api/procted/update', {
         method: 'POST',   // sending data to server backend 
         headers: {
           'Content-Type': 'application/json'
